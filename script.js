487f3c4fa0f5c8d23a3a4e32f200548f0006cbd4
Pat Castro <josuahcastro1@gmail.com>
2023-11-11 17:41:46 +0800

    added nav scroll animation

---
+++ script.js
@@ -0,0 +1,12 @@
+var lastScrollTop = 0;
+    navbar = document.getElementById("nav");
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document
+    .documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+        navbar.style.top = "-50px";
+    }else{
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+})
