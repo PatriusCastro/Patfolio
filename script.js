600d4903ccc354f1d28ad32e09eb5bd6bbaef029
Pat Castro <josuahcastro1@gmail.com>
2023-11-12 21:36:24 +0800

    finalize color palette

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", function(){
     var scrollTop = window.pageYOffset || document
     .documentElement.scrollTop;
     if(scrollTop > lastScrollTop){
-        navbar.style.top = "-50px";
+        navbar.style.top = "-60px";
     }else{
         navbar.style.top = "0";
     }
