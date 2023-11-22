b9579428dab4e78a3b8a6e238223fc9b98f21e73
Pat Castro <josuahcastro1@gmail.com>
2023-11-22 15:16:32 +0800

    active function not done

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ window.addEventListener("scroll", function(){
     }
     lastScrollTop = scrollTop;
 })
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('nav ul a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('nav ul a[href*= ' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
