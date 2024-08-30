a92853d6d9f1a320744ec8a2057212e79ce35807
Pat Castro <josuahcastro1@gmail.com>
2024-08-31 01:20:51 +0800

    Website Done!

--- script.js
+++ script.js
@@ -11,21 +11,9 @@ window.addEventListener("scroll", function(){
     lastScrollTop = scrollTop;
 })
 
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('nav ul a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('nav ul a[href*= ' + id + ']').classList.add('active');
-            });
-        };
-    });
-};  
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
